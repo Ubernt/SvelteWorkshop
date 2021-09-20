5523e6327e83f0adddd9744f742384087f5e580f
Jon Koivula <jonkoivula@hotmail.com>
2021-09-20 18:57:49 +0300

    Add asp.net core project with svelte

---
+++ SvelteApp/Pages/Home/Svelte.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SvelteApp.Pages.Home
+{
+    public class SvelteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
